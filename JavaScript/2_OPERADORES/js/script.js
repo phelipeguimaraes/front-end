4573d1582782781fb5fb52ac2e5e0d4f69f77c76
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-09 17:45:50 -0300

    Interpolação

--- JavaScript/2_OPERADORES/js/script.js
+++ JavaScript/2_OPERADORES/js/script.js
@@ -2,7 +2,7 @@
 console.log(typeof 2);
 console.log(typeof 14.3);
 console.log(typeof -23);
-console.log("----------------------------")
+console.log("----------------------------");
 
 // 2 - Operações Aritméticas
 console.log(2 + 4);
@@ -10,14 +10,14 @@ console.log(10 - 5);
 console.log(5 * 4);
 console.log(9 / 3);
 console.log(5 + (4 * 2));
-console.log("----------------------------")
+console.log("----------------------------");
 
 // 3 - Special numbers
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
 console.log( 12 * "oxi");
 console.log(typeof NaN);
-console.log("----------------------------")
+console.log("----------------------------");
 
 // 4 - Strings
 console.log("textos");
@@ -27,12 +27,16 @@ console.log('23')
 console.log(typeof "textos");
 console.log(typeof `Mais um texto`);
 console.log(typeof '23')
-console.log("----------------------------")
+console.log("----------------------------");
 
 // 5 - Símbolos especiais em string
 console.log("Testando a \n quebra de linha");
 console.log("Espaçamento \t de tab");
-console.log("----------------------------")
+console.log("----------------------------");
 
 // 6 - Concatenação
-console.log("Oi" + " tudo bem?" + " fazendo?")
+console.log("Oi" + " tudo bem?" + " fazendo?");
+
+// 7 - Template Strings
+console.log(`A soma de 2 + 2 é: ${2 + 2}`);
+console.log(`Podemos executar qualquer coisa aqui ${console.log("testando")}`);
