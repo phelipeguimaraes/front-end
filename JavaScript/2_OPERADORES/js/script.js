f5b1f3e33d05782a0a1c563bcccbdea9f5295029
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-09 15:58:45 -0300

    Operações Aritméticas

--- JavaScript/2_OPERADORES/js/script.js
+++ JavaScript/2_OPERADORES/js/script.js
@@ -2,3 +2,11 @@
 console.log(typeof 2);
 console.log(typeof 14.3);
 console.log(typeof -23);
+
+// 2 - Operações Aritméticas
+console.log(2 + 4);
+console.log(10 - 5);
+console.log(5 * 4);
+console.log(9 / 3);
+console.log(5 + (4 * 2));
+
