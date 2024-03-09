e50087b805cc7bf437fa6c18952470f9cdc91513
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-09 16:30:25 -0300

    Strings

--- JavaScript/2_OPERADORES/js/script.js
+++ JavaScript/2_OPERADORES/js/script.js
@@ -2,6 +2,7 @@
 console.log(typeof 2);
 console.log(typeof 14.3);
 console.log(typeof -23);
+console.log("----------------------------")
 
 // 2 - Operações Aritméticas
 console.log(2 + 4);
@@ -9,9 +10,21 @@ console.log(10 - 5);
 console.log(5 * 4);
 console.log(9 / 3);
 console.log(5 + (4 * 2));
+console.log("----------------------------")
 
 // 3 - Special numbers
 console.log(typeof Infinity);
 console.log(typeof -Infinity);
 console.log( 12 * "oxi");
 console.log(typeof NaN);
+console.log("----------------------------")
+
+// 4 - Strings
+console.log("textos");
+console.log(`Mais um texto`);
+console.log('23')
+
+console.log(typeof "textos");
+console.log(typeof `Mais um texto`);
+console.log(typeof '23')
+console.log("----------------------------")
