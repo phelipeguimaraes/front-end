42a55fc6d17380682eddec3bbb6492ef6efa57ad
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-14 17:48:33 -0300

    Aprofundando em variáveis

--- JavaScript/3_ESTRUTURAS/js/script.js
+++ JavaScript/3_ESTRUTURAS/js/script.js
@@ -12,3 +12,19 @@ console.log(idade)
 console.log(typeof nome)
 console.log(typeof idade)
 console.log('-------------------------------------')
+
+// 2 - mais sobre variáveis
+// let 2teste = 'inválido'
+//let @teste = 'inválido'
+
+let a = 10, b = 20, c = 30
+console.log(a, b, c)
+
+const namecomplet = 'Phelipe Vieira Guimarães'
+const nameComplet = 'Zeca Pagodinho'
+console.log(namecomplet)
+console.log(nameComplet)
+
+let _teste = 'ok'
+let $teste = 'ok tbm'
+console.log(_teste, $teste)
