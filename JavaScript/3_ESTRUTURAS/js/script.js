8d4e4141de2b7eb363117294b31f81e794348a85
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-14 17:20:29 -0300

    Salvando valores

---
+++ JavaScript/3_ESTRUTURAS/js/script.js
@@ -0,0 +1,14 @@
+// 1 - Variáveis
+let nome = 'Phelipe'
+
+console.log('Phelipe')
+console.log(nome)
+
+nome = 'Phelipe Guimarães'
+console.log(nome)
+
+const idade = 19
+console.log(idade)
+console.log(typeof nome)
+console.log(typeof idade)
+console.log('-------------------------------------')
