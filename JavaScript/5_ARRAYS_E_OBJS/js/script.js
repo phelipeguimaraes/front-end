cae7f708313b82821da7689f2a2c705598213615
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-03-25 09:39:41 -0300

    propriedades

--- JavaScript/5_ARRAYS_E_OBJS/js/script.js
+++ JavaScript/5_ARRAYS_E_OBJS/js/script.js
@@ -5,3 +5,17 @@
 
  const itens = ['Phelipe', true, 23.4]
  console.log(itens)
+ console.log('------------------')
+
+ // 2 - mais sobre arrays
+ const arr = ['a', 'b', 'c', 'd', 'e']
+ console.log(arr[3])
+ console.log(arr[43])
+
+ // 2 - propriedades
+ const numbers = [5, 3 ,4]
+ console.log(numbers.length)
+ console.log(numbers['length'])
+
+ const myName = 'Phelipe'
+ console.log(myName.length)
