a31c77e9f868842550c5bf7b59c64fe6af494dcc
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-06 17:29:45 -0300

    Métodos

---
+++ JavaScript/6_POO/js/script.js
@@ -0,0 +1,12 @@
+// 1 - métodos
+const animal = {
+    nome: 'Bob',
+    latir: function() {
+        console.log('au au')
+    }
+}
+
+console.log(animal)
+console.log(animal.nome)
+animal.latir()
+console.log('-------------------------')
