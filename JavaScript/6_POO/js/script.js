b70e860a03deb24e9440e85f53550cc1d70d6b9a
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-06 18:27:48 -0300

    Aprofundando em métodos

--- JavaScript/6_POO/js/script.js
+++ JavaScript/6_POO/js/script.js
@@ -10,3 +10,45 @@ console.log(animal)
 console.log(animal.nome)
 animal.latir()
 console.log('-------------------------')
+
+// 2 - aprofundando em métodos
+const pessoa = {
+    nome: 'Phelipe',
+    getNome: function() {
+        return this.nome
+    },
+
+    setNome: function(novoNome) {
+        this.nome = novoNome
+    }
+}
+console.log(pessoa.nome)
+console.log(pessoa.getNome())
+pessoa.setNome('Guilherme')
+console.log(pessoa.getNome())
+console.log('-------------------------')
+
+const veiculo = {
+    nome: 'Onix',
+    modelo: 2019,
+    câmbio: 'Automático',
+    KM: 50000,
+    getModel: function() {
+       return this.modelo 
+    },
+    getCam: function() {
+        return this.câmbio
+    },
+    setCam: function(newCam) {
+        this.câmbio = newCam
+    }
+    
+}
+
+console.log(veiculo)
+console.log(veiculo.getModel())
+console.log(veiculo.getCam())
+veiculo.setCam('Manual')
+console.log(veiculo.getCam())
+console.log(veiculo.getCam(veiculo.setCam('Câmbio automático')))
+console.log('-------------------------')
