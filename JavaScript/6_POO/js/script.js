aae52df8292ac18244f2471c8562c6bf11d9d9cc
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-06 22:15:39 -0300

    mais sobre prototypes

--- JavaScript/6_POO/js/script.js
+++ JavaScript/6_POO/js/script.js
@@ -63,3 +63,18 @@ console.log(Object.getPrototypeOf(bool))
 const arr = []
 console.log(arr.length)
 console.log(Object.getPrototypeOf(arr))
+console.log('-------------------------')
+
+// 4 - mais sobre prototype
+const myObject = {
+    a: 'b'
+}
+console.log(Object.getPrototypeOf(myObject))
+console.log(Object.getPrototypeOf(myObject) === Object.prototype)
+console.log('-------------------------')
+
+const mySecondObject = Object.create(myObject)
+console.log(mySecondObject)
+console.log(mySecondObject.a)
+console.log(Object.getPrototypeOf(mySecondObject) === myObject)
+console.log('-------------------------')
