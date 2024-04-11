f514ee111dbad52813ede72413e9cc9ccc7e4b24
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-11 17:31:43 -0300

    Método debug: console.log

--- JavaScript/7_DEBUG/js/script.js
+++ JavaScript/7_DEBUG/js/script.js
@@ -8,3 +8,23 @@ const opa = 'teste' // melhor forma de variar
 
 // delete[].length
 console.log('----------')
+
+// 2 -  console.log
+let a = 1
+let b = 2
+
+if(a == 1) {
+    a = b + 2
+}
+console.log(a)
+
+for(let i = 0; i < b; i++) {
+    a = a + 2
+    console.log(a)
+}
+
+if(a > 5) {
+    a = 25
+}
+console.log(a)
+console.log('----------')
