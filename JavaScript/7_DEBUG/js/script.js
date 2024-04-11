6c29f3f059ea72f92f4ffe061800e2a74a16345f
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-11 18:01:08 -0300

    debugger

--- JavaScript/7_DEBUG/js/script.js
+++ JavaScript/7_DEBUG/js/script.js
@@ -28,3 +28,23 @@ if(a > 5) {
 }
 console.log(a)
 console.log('----------')
+
+// 3 - debugger
+let c = 1
+let d = 2
+
+if(c == 1) {
+    c = d + 2
+}
+
+debugger
+for(let i =  0; i < d; i++) {
+    c = c + 2
+}
+
+console.log('executou o loop')
+debugger
+if(c > 5) {
+    c = 25
+}
+console.log('----------')
