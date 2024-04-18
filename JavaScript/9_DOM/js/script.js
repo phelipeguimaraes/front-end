63949d446f644c0b9c41fad824d4d4cfaaaacbf8
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-18 20:52:04 -0300

    selecionando uma tag

--- JavaScript/9_DOM/js/script.js
+++ JavaScript/9_DOM/js/script.js
@@ -3,3 +3,7 @@ console.log(document.body)
 console.log(document.body.childNodes[1])
 console.log(document.body.childNodes[1].childNodes)
 console.log(document.body.childNodes[1].childNodes[1].textContent)
+
+// 2 - selecionando por tag
+const listaItens = document.getElementsByTagName('li')
+console.log(listaItens)
