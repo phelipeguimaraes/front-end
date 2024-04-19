12928dfc0516314d3bd2420872d0b25408422819
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-18 21:00:43 -0300

    selecionando elemento por id

--- JavaScript/9_DOM/js/script.js
+++ JavaScript/9_DOM/js/script.js
@@ -7,3 +7,7 @@ console.log(document.body.childNodes[1].childNodes[1].textContent)
 // 2 - selecionando por tag
 const listaItens = document.getElementsByTagName('li')
 console.log(listaItens)
+
+// 3 - selecionando elemento por id
+const title = document.getElementById("title")
+console.log(title)
