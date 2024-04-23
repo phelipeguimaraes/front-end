312ffb690bdc2d783e2b65445151badd1c8afe2d
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-04-23 18:36:27 -0300

    propagação

--- JavaScript/10_EVENTOS/js/script.js
+++ JavaScript/10_EVENTOS/js/script.js
@@ -23,7 +23,19 @@ thirdBtn.addEventListener('click', () => {
 const myTitle = document.querySelector('#my-title')
 myTitle.addEventListener('click', (event) => {
     console.log(event)
-    console.log(event.offsetX)
-    console.log(event.pointerType)
-    console.log(event.target)
+    console.log(event.offsetX) // posição do elemento no eixo X
+    console.log(event.pointerType) // tipo de click, touch ou mouse
+    console.log(event.target) // informação do elemento
+})
+
+// 4 - propagação
+const containerBtn = document.querySelector('#btn-container')
+const btnInsideContainer = document.querySelector('#div-btn')
+
+containerBtn.addEventListener('click', () => {
+    console.log('Evento 1')
+})
+btnInsideContainer.addEventListener('click', (e) => {
+    e.stopPropagation() // exclusivamente vai executar só esse EVENTO! se clicar nele
+    console.log('Evento 2')
 })
