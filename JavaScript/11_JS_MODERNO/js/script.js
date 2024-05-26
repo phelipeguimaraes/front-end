73129221ad9870140a1bf13d1a3731a8c119e5fc
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-05-25 21:14:00 -0300

    Arrow function ES+

--- JavaScript/11_JS_MODERNO/js/script.js
+++ JavaScript/11_JS_MODERNO/js/script.js
@@ -26,3 +26,44 @@ function logName() {
 const name = 'Guilherme'
 logName()
 console.log(name)
+console.log('--------------------------------') 
+
+// 2 - arrow function
+const sum =  function(a, b) {
+    return a + b
+}
+
+const arrowSum = (a, b) => a + b
+
+console.log(sum(5, 5))
+console.log(arrowSum(5, 5))
+
+const greeting = (name) => {
+    if(name) {
+        return `Hello ${name}`
+    } else {
+        return 'Hello'
+    }
+}
+
+console.log(greeting('Phelipe'))
+console.log(greeting())
+
+const user = {
+    name: 'Theo',
+    sayUserName() {
+        setTimeout(function() {
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        },  1000)
+    },
+    sayUserNameArrow() {
+        setTimeout(() => {
+            console.log(this)
+            console.log(`Username: ${this.name}`)
+        }, 2000)
+    }
+
+}
+user.sayUserName()
+user.sayUserNameArrow()
