1b28b45e4f712e8417ce2e9a4dfbbc8f6addaba4
Phelipe Vieira Guimarães <137660442+phelipeguimaraes@users.noreply.github.com>
2024-06-05 18:46:50 -0300

    primeiro request

--- JavaScript/12_AXIOS/js/script.js
+++ JavaScript/12_AXIOS/js/script.js
@@ -1,2 +1,17 @@
 // 1 - instalação
-console.log(axios)
+/*console.log(axios)*/
+
+// 2 - primeiro request
+const getData = async() => {
+    try {
+        const response = await axios.get("https://jsonplaceholder.typicode.com/users")
+
+        console.log(response)
+        return response.data
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getData()
