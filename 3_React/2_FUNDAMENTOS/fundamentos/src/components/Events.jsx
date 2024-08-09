9fa9f941bc22527b0b02903ff0c3b57362cee902
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-08-09 19:34:10 -0300

    função com renderização

--- 3_React/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
+++ 3_React/2_FUNDAMENTOS/fundamentos/src/components/Events.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
 const Events = () => {
+
+    const handleClick = (e) => {
+        console.log(e)
+        console.log('executou')
+    }
+
+    // 8 - Função de renderização
+    const renderSomething = (x) => {
+        if(x) {
+            return <h1>Renderizando isso!</h1>
+        } else {
+            return <h1>Renderuzando outra coisa!</h1>
+        }
+    }
+
+    // return 10 > 11 && <p>Carregando...</p>
+
   return (
     <div>
-        <button onClick={() => alert("Testando minha aplicação")}>Clique aqui</button>
+        <div>
+            <button onClick={() => alert("Testando minha aplicação")}>Clique aqui</button>
+        </div>
+        {/* 7 - evento com função */}
+        <div>
+            <button onClick={handleClick}>Clique aqui - com função</button>
+        </div>
+        {/* 8 - Função com render */}
+        {renderSomething(true)}
+        {renderSomething(false)}
     </div>
   )
 }
