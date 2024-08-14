3850fd1f1a3ea33146ad38d7843ce00df53a65fd
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-08-14 19:08:25 -0300

    propriedade key

--- 3_React/3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
+++ 3_React/3_AVANCANDO_EM_REACT/avancando/src/components/ListRender.jsx
@@ -2,15 +2,33 @@ import { useState } from "react"
 
 const ListRender = () => {
 
-    const [list] = useState(["Phelipe", "Dudu", "Guilherme"])
+    const [list] = useState(["Phelipe", "Dudu", "Guilherme", "Dudu"])
+
+    const [users, setUsers] = useState([
+      {id: 1, name: "Phelipe", age: 20},
+      {id: 2, name: "Dudu", age: 19},
+      {id: 3, name: "Lara", age: 15}
+    ])
 
   return (
     <div>
-        <ul>
-           {list.map((item) =>(
-            <li>{item}</li>
-           ))}
-        </ul>
+
+        {/* 4 - render sem key */}
+          <ul>
+            {list.map((item, i) => (
+              <li key={i}>{item}</li>
+            ))}
+          </ul>
+
+        {/* 5 - render com key */}
+          <ul>
+            {users.map((user) => (
+              <li key={user.id}>
+                {user.name} - {user.age} anos
+              </li>
+            ))}
+          </ul>
+
     </div>
   )
 }
