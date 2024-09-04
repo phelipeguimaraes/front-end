85035e0561f9f4d161e30f489bff2b7160c3a351
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-09-04 15:24:25 -0300

    Manipulando valores

--- 3_React/5_FORM_REACT/form/src/components/MyForm.jsx
+++ 3_React/5_FORM_REACT/form/src/components/MyForm.jsx
@@ -1,13 +1,31 @@
 import "./MyForm.css"
 
+import { useState } from "react";
+
 const MyForm = () => {
+
+    // 3 - gerenciamento de dados
+    const [name, setName] = useState()
+    const [email, setEmail] = useState()
+
+    const handleName = (e) => {
+        setName(e.target.value)
+    }
+
+    console.log(name)
+
   return (
     <div>
         {/* 1 - Criação de form */}
         <form>
             <div>
                 <label htmlFor="name">Nome:</label>
-                <input type="text" name="name" placeholder="Digite o seu nome" />
+                <input
+                 type="text"
+                 name="name" 
+                 placeholder="Digite o seu nome"  
+                 onChange={handleName}
+                 />
             </div>
             
             {/* 2 - label envolvendo input */}
