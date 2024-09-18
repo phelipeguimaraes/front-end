01abae2785bc5689a57bb82475f1ba7d101ad437
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-09-18 19:38:03 -0300

    Resgatando dados com React

---
+++ 3_React/6_HTTP_REACT/http_react/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect } from "react";
+
+const url = "http://localhost:3000/products"
+
+import './App.css'
+
+function App() {
+  // 1 -  Resgatando dados
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+
+    async function getData() {
+
+      const res = await fetch(url)
+
+      const data = await res.json()
+
+      setProducts(data)
+    }
+
+    getData()
+
+  }, [])
+  
+  return (
+    <>
+      <div>
+        <h1>HTTP em React</h1>
+        {/* 1 - resgate de dados */}
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R${product.price}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
+  )
+}
+
+export default App
