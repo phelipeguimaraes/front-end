7632eb6f66deb065c0923f0c4d2a6f52cb7bffe6
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-10-27 22:03:19 -0300

    Configurando o React Router

--- 3_React/7_REACT_ROUTER/router/src/main.jsx
+++ 3_React/7_REACT_ROUTER/router/src/main.jsx
@@ -3,8 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import Contact from './routes/Contact.jsx'
+
+import { createBrowserRouter, RouterProvider, Router } from "react-router-dom"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "contact",
+    element: <Contact />,
+  },
+])
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
