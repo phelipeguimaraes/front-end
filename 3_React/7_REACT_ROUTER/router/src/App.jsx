1b376e81d8ba49e10a387eecdfe73de9c175bd6b
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-10-30 17:54:04 -0300

    Links entre páginas

--- 3_React/7_REACT_ROUTER/router/src/App.jsx
+++ 3_React/7_REACT_ROUTER/router/src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 import { Outlet } from 'react-router-dom'
 
+// 5 - link entre paginas
+import Navbar from './components/Navbar'
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
       <div>
+        <Navbar />
         <p>Navbar</p>
         <Outlet/>
         <p>Footer</p>
