09152356cd904a811f2ce890ff7edf3ad50d274f
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-10-30 18:48:36 -0300

    Carregamento de dados

--- 3_React/7_REACT_ROUTER/router/src/routes/Home.jsx
+++ 3_React/7_REACT_ROUTER/router/src/routes/Home.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 
-const Home = () => {
+// 6 - carregando dados
+import {useFetch} from '../hooks/useFetch'
+
+const url = "http://localhost:3000/products"
+
+const Home = () => {  
+  const {data: items} = useFetch(url)
+
+
   return (
     <div>
         <h1>Home</h1>
+        {/* 6 - carregamento de dados */}
+        <ul className="products">
+          {items && items.map((item) => (
+            <li key={item.id}>
+              <h2>{item.name}</h2>
+              <p>R$: {item.price}</p>
+            </li>
+          ))}
+        </ul>
     </div>
   )
 }
