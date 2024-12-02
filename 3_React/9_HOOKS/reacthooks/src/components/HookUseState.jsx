64c34662176356d06212ae0a6b94b71a921b88c6
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-12-02 17:52:40 -0300

    useState e inputs

--- 3_React/9_HOOKS/reacthooks/src/components/HookUseState.jsx
+++ 3_React/9_HOOKS/reacthooks/src/components/HookUseState.jsx
@@ -7,15 +7,24 @@ const HookUseState = () => {
     // 1 - useState
     let userName = "João"
     const [name, setName] = useState("Phelipe")
-    const [age, setAge] = useState(19)
+    //const [age, setAge] = useState(19)
 
     const changeNames = () => {
         userName = 'Pedro Alves'
         setName("Phelipe Guimarães")
     }
 
-    const changeAge = () => {
-        setAge(20)
+    //const changeAge = () => {
+    //    setAge(20)
+    //}
+
+    // 2 - useState e input
+    const [age, setAge] = useState(19)
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        console.log(age)
     }
 
   return (
@@ -24,10 +33,21 @@ const HookUseState = () => {
         <h2>useState</h2>
         <p>Variável: {userName}</p>
         <p>useState: {name}</p>
-        <p>useState: {age}</p>
 
         <button onClick={changeNames}>Mudar nomes</button>
-        <button onClick={changeAge}>Mudar idade</button>
+        {/*<button onClick={changeAge}>Mudar idade</button>*/}
+
+        {/* 2 - useState e input */}
+        <form onSubmit={handleSubmit}>
+            <input
+              type="text" 
+              value={age} 
+              onChange={(e) => setAge(e.target.value)}
+            />
+            <input type="submit" value="Enviar" />
+        </form>
+        <p>Você tem {age} anos.</p>
+        <hr />
     </div>
   )
 }
