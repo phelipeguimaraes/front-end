a282136d5d66158565d0a1932b691cc6b54dd82f
phelipeguimaraes <phelipeguimaraes2023@gmail.com>
2024-12-15 00:45:04 -0300

    custom hook

---
+++ 3_React/9_HOOKS/reacthooks/src/components/HookCustom.jsx
@@ -0,0 +1,20 @@
+import {useState} from 'react'
+import { usePrevious } from './usePrevious'
+
+
+const HookCustom = () => {
+
+    const [number, setNumber] = useState(0)
+    const previousValue = usePrevious(number)
+
+  return (
+    <div>
+        <h2>Custom hook</h2>
+        <p>Atual: {number}</p>
+        <p>Anterior: {previousValue}</p>
+        <button onClick={() => setNumber(Math.random())}>Alterar</button>
+    </div>
+  )
+}
+
+export default HookCustom
